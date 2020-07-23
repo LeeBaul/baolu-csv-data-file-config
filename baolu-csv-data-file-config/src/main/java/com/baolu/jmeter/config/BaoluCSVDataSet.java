9e8dc202e5393a2cfe1896259eb168f2dca2b479
宝路 <847586537@qq.com>
2020-07-24 00:28:34 +0800

    init baolu-jmeter-plugins project

---
+++ baolu-csv-data-file-config/src/main/java/com/baolu/jmeter/config/BaoluCSVDataSet.java
@@ -0,0 +1,12 @@
+package com.baolu.jmeter.config;
+
+/**
+ * <p/>
+ *
+ * @author libaolu
+ * @version 1.0
+ * @date 2020/7/23 22:34
+ **/
+public class BaoluCSVDataSet {
+
+}
---
+++ baolu-csv-data-file-config/src/main/java/com/baolu/jmeter/config/BaoluCSVDataSetBeanInfo.java
@@ -0,0 +1,12 @@
+package com.baolu.jmeter.config;
+
+/**
+ * <p/>
+ *
+ * @author libaolu
+ * @version 1.0
+ * @date 2020/7/23 22:35
+ **/
+public class BaoluCSVDataSetBeanInfo {
+
+}
