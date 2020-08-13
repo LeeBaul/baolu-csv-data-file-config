6d9c66eee023073b651689971f0af1be497dad7a
libaolu <847586537@qq.com>
2020-08-13 14:25:43 +0800

    增加依赖 初始化代码

--- baolu-csv-data-file-config/src/main/java/com/baolu/jmeter/config/BaoluCSVDataSet.java
+++ baolu-csv-data-file-config/src/main/java/com/baolu/jmeter/config/BaoluCSVDataSet.java
@@ -1,5 +1,11 @@
 package com.baolu.jmeter.config;
 
+import org.apache.jmeter.config.ConfigTestElement;
+import org.apache.jmeter.engine.event.LoopIterationEvent;
+import org.apache.jmeter.engine.event.LoopIterationListener;
+import org.apache.jmeter.testbeans.TestBean;
+import org.apache.jmeter.testelement.TestStateListener;
+
 /**
  * <p/>
  *
@@ -7,6 +13,25 @@ package com.baolu.jmeter.config;
  * @version 1.0
  * @date 2020/7/23 22:34
  **/
-public class BaoluCSVDataSet {
+public class BaoluCSVDataSet extends ConfigTestElement implements TestBean,LoopIterationListener, TestStateListener {
 
+    public void iterationStart(LoopIterationEvent loopIterationEvent) {
+
+    }
+
+    public void testStarted() {
+
+    }
+
+    public void testStarted(String s) {
+
+    }
+
+    public void testEnded() {
+
+    }
+
+    public void testEnded(String s) {
+
+    }
 }
