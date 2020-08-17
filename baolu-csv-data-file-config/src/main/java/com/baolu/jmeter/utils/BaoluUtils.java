aecf174a852e21b843c25c679e3d8763f14ed969
libaolu <847586537@qq.com>
2020-08-17 15:15:06 +0800

    获取线程索引

--- baolu-csv-data-file-config/src/main/java/com/baolu/jmeter/utils/BaoluUtils.java
+++ baolu-csv-data-file-config/src/main/java/com/baolu/jmeter/utils/BaoluUtils.java
@@ -13,5 +13,18 @@ public class BaoluUtils {
 
     }
 
+    public static int getThreadIndex(String JMeterThreadName){
+        int index = JMeterThreadName.lastIndexOf(" ");
+        if (index > 0){
+            String tmpStr = JMeterThreadName.substring(index + 1);
+            String args[] = tmpStr.split("-");
+            index = Integer.valueOf(args[1]);
+        }
+        return index;
+    }
 
+    public static void main(String[] args) {
+        int index = BaoluUtils.getThreadIndex("bzm - Arrivals Thread Group 1-1");
+        System.out.println(index);
+    }
 }
