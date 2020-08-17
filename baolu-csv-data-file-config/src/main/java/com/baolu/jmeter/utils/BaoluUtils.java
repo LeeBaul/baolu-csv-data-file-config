588d60760567226c311597bd2c702d093fe840f2
libaolu <847586537@qq.com>
2020-08-17 14:06:29 +0800

    新增工具类

---
+++ baolu-csv-data-file-config/src/main/java/com/baolu/jmeter/utils/BaoluUtils.java
@@ -0,0 +1,17 @@
+package com.baolu.jmeter.utils;
+
+/**
+ * <p/>
+ * 工具类
+ * @author libaolu
+ * @version 1.0
+ * @dateTime 2020/8/17 14:01
+ **/
+public class BaoluUtils {
+
+    public BaoluUtils(){
+
+    }
+
+
+}
