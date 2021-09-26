8fe024b4690b0b81d3b966a5276bfb720a3f9890
libaolu <847586537@qq.com>
2021-09-26 19:30:52 +0800

    jmeter agent init

---
+++ baolu-jmeter-instrument-agent/src/main/java/com/libaolu/jmeter/instrument/agent/JMeterAgent.java
@@ -0,0 +1,58 @@
+package com.libaolu.jmeter.instrument.agent;
+
+import org.apache.jmeter.JMeter;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * <p/>
+ *
+ * @author libaolu
+ * @version 1.0
+ * @dateTime 2021/9/26 16:48
+ **/
+public class JMeterAgent {
+    /**
+     * jvm 参数形式启动，运行此方法
+     * @param agentArgs
+     * @param inst
+     */
+    public static void premain(String agentArgs, Instrumentation inst){
+        System.out.println("premain");
+        System.out.println(agentArgs);
+        customLogic(inst);
+    }
+
+    /**
+     * 动态 attach 方式启动，运行此方法
+     * @param agentArgs
+     * @param inst
+     */
+    public static void agentmain(String agentArgs, Instrumentation inst){
+        System.out.println("agentmain");
+        inst.addTransformer(new JMeterTransformer(), true);
+        try {
+            //重定义类并载入新的字节码
+            inst.retransformClasses(JMeter.class);
+            System.out.println("JMeterAgent Load Done.");
+        } catch (Exception e) {
+            System.out.println("JMeterAgent load failed!");
+        }
+    }
+
+    /**
+     * 打印所有已加载的类名称
+     * 修改字节码
+     * @param inst
+     */
+    private static void customLogic(Instrumentation inst){
+        inst.addTransformer(new JMeterTransformer(), true);
+        try {
+            //重定义类并载入新的字节码
+            inst.retransformClasses(JMeter.class);
+            System.out.println("JMeterAgent Load Done.");
+        } catch (Exception e) {
+            System.out.println("JMeterAgent load failed!");
+        }
+    }
+}
