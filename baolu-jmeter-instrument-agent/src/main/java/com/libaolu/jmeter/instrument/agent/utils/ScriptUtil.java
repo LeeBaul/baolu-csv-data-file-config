28a7dba6a22b302c3572d6ea63e4f07919815e17
libaolu <847586537@qq.com>
2021-09-28 22:30:18 +0800

    方法体

--- baolu-jmeter-instrument-agent/src/main/java/com/libaolu/jmeter/instrument/agent/utils/ScriptUtil.java
+++ baolu-jmeter-instrument-agent/src/main/java/com/libaolu/jmeter/instrument/agent/utils/ScriptUtil.java
@@ -33,4 +33,56 @@ public class ScriptUtil {
             e.printStackTrace();
         }
     }
+
+    public static String getEnhancedMethodBody(){
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("for (Object o : new ArrayList<>(tree.list())) {");
+            buffer.append("if (o instanceof TestElement) {");
+                buffer.append("TestElement item = (TestElement) o;");
+                buffer.append("if (item.isEnabled()) {");
+                    buffer.append("if (item instanceof ReplaceableController) {");
+                        buffer.append("ReplaceableController rc = ensureReplaceableControllerIsLoaded(item);");
+                        buffer.append("HashTree subTree = tree.getTree(item);");
+                        buffer.append("if (subTree != null) {");
+                            buffer.append("HashTree replacementTree = rc.getReplacementSubTree();");
+                            buffer.append("if (replacementTree != null) {");
+                                buffer.append("pConvertSubTree(replacementTree);");
+                                buffer.append("tree.replaceKey(item, rc);");
+                                buffer.append("tree.set(rc, replacementTree);");
+                            buffer.append("}");
+                        buffer.append("}");
+                    buffer.append("} else { ");
+                        buffer.append("pConvertSubTree(tree.getTree(item)); ");
+                    buffer.append("}");
+                buffer.append("} else {");
+                    buffer.append(" tree.remove(item);");
+                buffer.append("}");
+            buffer.append("} else {");
+                buffer.append("JMeterTreeNode item = (JMeterTreeNode) o;");
+                buffer.append("if (item.isEnabled()) {");
+                    buffer.append("if (item.getUserObject() instanceof ReplaceableController) {");
+                        buffer.append("TestElement controllerAsItem = item.getTestElement();");
+                        buffer.append("ReplaceableController rc = ensureReplaceableControllerIsLoaded(controllerAsItem);");
+                        buffer.append("HashTree subTree  =  tree.getTree(item);");
+                        buffer.append("if (subTree  !=  null) {");
+                            buffer.append("HashTree replacementTree = rc.getReplacementSubTree();");
+                            buffer.append("if (replacementTree != null) {");
+                                buffer.append("pConvertSubTree(replacementTree);");
+                                buffer.append("tree.replaceKey(item, rc);");
+                                buffer.append("tree.set(rc, replacementTree);");
+                            buffer.append("}");
+                        buffer.append("}");
+                    buffer.append("} else {");
+                        buffer.append("pConvertSubTree(tree.getTree(item));");
+                        buffer.append("TestElement testElement = item.getTestElement();");
+                        buffer.append("tree.replaceKey(item, testElement);");
+                    buffer.append("}");
+                buffer.append("} else {");
+                    buffer.append(" tree.remove(item);");
+                buffer.append("}");
+            buffer.append("}");
+        buffer.append("}");
+        return buffer.toString();
+    }
+
 }
